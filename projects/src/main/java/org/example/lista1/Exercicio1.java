c711ac78dc29bc3ef987d58d5f0c79c58c0ad04a
Jean <{99772124}+{jeanmends}@users.noreply.github.com>
2025-12-12 20:30:15 -0300

    project created

---
+++ projects/src/main/java/org/example/lista1/Exercicio1.java
@@ -0,0 +1,6 @@
+package org.example.lista1;
+
+public class Exercicio1 {
+    //Escreva um código que receba o nome e o ano de nascimento de alguém e imprima
+    //na tela a seguinte mensagem: "Olá 'Fulano' você tem 'X' anos"
+}
